78ccc4916b457b43b3f48978836d837a5631488b
pratyushvid3105 <pratyush310599@gmail.com>
2025-01-07 16:47:48 +0530

    final commit

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -10,7 +10,7 @@ module.exports = {
   devtool: "cheap-module-source-map",
   plugins: [
     new webpack.DefinePlugin({
-      "process.env.name": JSON.stringify("Pratyush"),
+      "process.env.name": JSON.stringify("webpack dev"),
     }),
     new ReactRefreshWebpackPlugin(),
   ],
--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -6,7 +6,7 @@ module.exports = {
   devtool: "source-map",
   plugins: [
     new webpack.DefinePlugin({
-      "process.env.name": JSON.stringify("Pratyush Vidyarthi"),
+      "process.env.name": JSON.stringify("webpack prod"),
     }),
     new BundleAnalyzerPlugin(),
   ],
